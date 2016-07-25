36aa989c6c08af4be17980dc8b1cb2dc9de70553
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-25 09:18:34 +0200

    Update core.js

--- core.js
+++ core.js
@@ -2,14 +2,14 @@ var canvas, map, setup, xhr, players;
 var xhr = new XMLHttpRequest();
 
 function onHttpAnswer() {
-  if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
     var response = xhr.responseText;
     if (request == "firstSetup") {
       var jsonResponse = JSON.parse(response);
       console.log(response);
       console.log(jsonResponse);
     }
-  } else {
+  } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status != 200) {
     console.log(xhr);
   }
 }
