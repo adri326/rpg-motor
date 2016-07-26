47fc2f34bd64c4defb6835c553b3bab3c092d186
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-27 00:10:19 +0200

    Update createMap.php

--- createMap.php
+++ createMap.php
@@ -8,16 +8,16 @@
       var starter = "ressources/images/theme/basic-16/";
       function changeValue(y, x) {
         var img = document.getElementById(y+"-"+x);
-        img.src = starter+prompt('What value to set?", "0";
+        img.src = starter+prompt('What value to set?", "0");
       }
     </script>
   </head>
   <body>
     <table>
       <?php
-        for ($y = 0; y < $_GET['height']; y++) {
+        for ($y = 0, y < $_GET['height'], y++) {
           echo '<tr>';
-          for ($x = 0; x < $_GET['width']; x++) {
+          for ($x = 0, x < $_GET['width'], x++) {
             echo '<td><img id="'.$y.'-'.$x.'" src="ressources/images/theme/basic-16/0.png" alt="0" onclick="changeValue('.$y.', '.$x');" /></td>';
           }
           echo '</tr>';
