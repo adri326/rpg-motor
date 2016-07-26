70570b6cfe39cc9cfea289266ed18c95867e5888
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-27 00:15:52 +0200

    Update createMap.php

--- createMap.php
+++ createMap.php
@@ -15,9 +15,9 @@
   <body>
     <table>
       <?php
-        for ($y = 0, y < $_GET['height'], y++) {
+        for ($y = 0; $y < $_GET['height']; $y++) {
           echo '<tr>';
-          for ($x = 0, x < $_GET['width'], x++) {
+          for ($x = 0; $x < $_GET['width']; $x++) {
             echo '<td><img id="'.$y.'-'.$x.'" src="ressources/images/theme/basic-16/0.png" alt="0" onclick="changeValue('.$y.', '.$x');" /></td>';
           }
           echo '</tr>';
