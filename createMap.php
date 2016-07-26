426cb486d583863bf5963aad4c7c365cb1f01f03
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-26 23:56:40 +0200

    Create createMap.php

---
+++ createMap.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Map editor</title>
+    <script>
+      var map = [[]];
+      var width, height;
+      var starter = "ressources/images/theme/basic-16/";
+      function changeValue(y, x) {
+        var img = document.getElementById(y+"-"+x);
+        img.src = starter+prompt('What value to set?", "0";
+      }
+    </script>
+  </head>
+  <body>
+    <table>
+      <?php
+        for ($y = 0; y < $_GET['height']; y++) {
+          echo '<tr>';
+          for ($x = 0; x < $_GET['width']; x++) {
+            echo '<td><img id="'.$y.'-'.$x.'" src="ressources/images/theme/basic-16/0.png" alt="0" onclick="changeValue('.$y.', '.$x');" /></td>';
+          }
+          echo '</tr>';
+        }
+      ?>
+    </table>
+  </body>
+</html>
