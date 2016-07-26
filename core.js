80be755a61f70e4fa7561c3c88d66410e7ec7198
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-26 14:30:59 +0200

    Update core.js

--- core.js
+++ core.js
@@ -2,6 +2,25 @@
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
   window.requestAnimationFrame = requestAnimationFrame;
   })();
+var vis = (function(){
+    var stateKey, eventKey, keys = {
+        hidden: "visibilitychange",
+        webkitHidden: "webkitvisibilitychange",
+        mozHidden: "mozvisibilitychange",
+        msHidden: "msvisibilitychange"
+    };
+    for (stateKey in keys) {
+        if (stateKey in document) {
+            eventKey = keys[stateKey];
+            break;
+        }
+    }
+    return function(c) {
+        if (c) document.addEventListener(eventKey, c);
+        return !document[stateKey];
+    }
+  })();
+
 var canvas, map, setup, xhr, players;
 var xhr = new XMLHttpRequest();
 var request = "firstSetup";
