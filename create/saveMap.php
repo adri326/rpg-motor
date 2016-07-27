b911ba59b5102b1dd708421dc58e9087418e27d7
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-28 01:36:33 +0200

    Update saveMap.php

--- create/saveMap.php
+++ create/saveMap.php
@@ -1,1 +1,10 @@
-
+<?php session_start();
+$party = sprintf('%04d', $_GET['party']);
+$player = sprintf('%04d', $_GET['player']);
+$mapId = $_GET['id']*;
+$map = $_GET['map'];
+$path = $__DIR__."../parties/".$party."/map/".$mapId.".json";
+$file = fopen($path, "w");
+fwrite($file, $map);
+fclose($file);
+?>
