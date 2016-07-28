35dbe912f1797f7cf4cd6173d1a562f9f8e0bb03
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-28 15:20:05 +0200

    Update createMap.php

--- create/createMap.php
+++ create/createMap.php
@@ -3,61 +3,7 @@
   <head>
     <title>Map editor</title>
     <script src="../drawMap.js"></script>
-    <script>
-      var map =  <?php
-        echo '[';
-        for ($y = 0; $y < $_GET['height']; $y++) {
-          echo '[';
-          for ($x = 0; $x < $_GET['width']; $x++) {
-            echo '[0]';
-            if ($x+1<$_GET['width']) {
-              echo ',';
-            }
-          }
-          echo ']';
-          if ($y+1<$_GET['height']) {
-            echo ',';
-          }
-        }
-        echo ']';
-        ?>;
-      var width, height;
-      var ix = 0, iy = 0;
-      var starter = "../ressources/images/theme/basic-16/";
-      function changeValue(y, x) {
-        var img = document.getElementById(y+"-"+x);
-        var nval = JSON.parse(prompt("What value to set?", "0"));
-        if (typeof nval == "array") {
-          img.src = starter+nval[0]+".png";
-          img.alt = nval[0];
-          map[y][x] = nval;
-        } else {
-          img.src = starter+nval+".png";
-          img.alt = nval;
-          map[y][x][0] = parseInt(nval, 10);
-        }
-      }
-      function changeSize() {
-        width = document.getElementById('width').value;
-        height = document.getElementById('height').value;
-        window.location = 'createMap.php?width='+width+'&height='+height;
-      }
-      function saveMap() {
-        var mapId = document.getElementById('mapId').value;
-        var party = document.getElementById('party').value;
-        window.location = 'saveMap.php?party='+party+'&mapId='+mapId+'&map='+JSON.stringify(map);
-      }
-      var canvas, ctx, tileSize = 32;
-      function init() {
-        canvas = document.getElementById('canvas');
-        ctx = canvas.getContext("2d");
-        draw();
-      }
-      function draw() {
-        drawMap();
-      }
-      window.addEventListener("load", init);
-      
+    <script src="core.js">
     </script>
     <style>
       .editor {
