2dfd5871eb1b4dd55711f986813de1ff07cdc648
_̜͚̪͍̣̞͉̦͇̞̼̤̱̭̃̒͊̑̄̌ͨ̋̓̍ͫͮ͊͐̏ͮ͒̚͜͜_̴͇̭̬̯ͬ̿͂̍̌͛͊͗̚_̂̇̃̉͊͗̐́ͥ͋ͩ̿́̿̚͞҉̷̨̛̮̲̝̣̬̮͓̩̳̥̥̙̪̫͙M̡̡̭͍̞͈̥̹̝̝̰̤͍̲̤͚̣̜͙͌̈́̎ͭͯ͜͟͞ͅE̴̡̟͎͇̖̟͙̩̺̒̅ͦ͊͐̋̇̓ͪ̽̈́̏ͫ̏ͥ̂_̶̧̨̛͕͖̣̯͔͖̩̲̻͍̝̰̹̘̟̱̞̬͗̄̏ͮͤ͝_͖̩̥͓̱̩̝̯̞̠̦͉͔̆͒̆ͯ̇͢͟͝_̢͙̺̳͖̝͕̭͔̓̽̋̏̇̾̄̃͋͟͜ <adrien.burgun@orange.fr>
2016-07-28 15:04:11 +0200

    Create drawMap.js

---
+++ drawMap.js
@@ -0,0 +1,25 @@
+function drawMap() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  width = canvas.width;
+  height = canvas.height;
+  
+  sx = Math.round(ix/tileSize-1.5);
+  sy = Math.round(iy/tileSize-1.5);
+  ex = Math.round((ix+width)/tileSize+0.5);
+  ey = Math.round((iy+height)/tileSize+0.5);
+  xo = ix%tileSize;
+  yo = iy%tileSize;
+  for (y = sy; y < ey; y++) {
+    for (x = sx; x < ex; x++) {
+      if (y>=0&&y<map.length) {
+        if (x>=0&&x<map[y].length) {
+          for (z = 0; z < map[y][x].length; z++) {
+            ctx.drawImage(image(map[y][x][z]), x*tileSize+xo-ix, y*tileSize+yo-iy, tileSize, tileSize);
+          }
+        }
+      }
+    }
+  }
+  requestAnimationFrame(draw);
+}
