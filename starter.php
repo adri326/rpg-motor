74c2845856814b84b66c1de270ee841205911350
_̦͍͇_͎͚̰͚_̠̜ME_̦͍͇_͎͚̰͚_̠̜ <adrien.burgun@orange.fr>
2016-07-29 00:01:09 +0200

    Create starter.php

---
+++ starter.php
@@ -0,0 +1,22 @@
+<header class="starter">
+  <div class="starter-container">
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <div id="menuList">
+              <ul id="menu">
+                <a href="/"><li>Home</li></a>
+              </ul>
+            </div>
+          </td>
+          <td>
+            <div id="title">
+              RPG - motor
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</header>
