97cbf32df208b471deee6ce224bc4cd001ff5f1a
_̦͍͇_͎͚̰͚_̠̜ME_̦͍͇_͎͚̰͚_̠̜ <adrien.burgun@orange.fr>
2016-07-30 22:01:31 +0200

    Programmed the main part of tileReader.js

--- tilesReader.js
+++ tilesReader.js
@@ -1,5 +1,21 @@
 var tiles;
 var tilesSaved = false;
 function saveTileJSON(input) {
-  
+  tiles = JSON.parse(input);
+}
+fonction getTileInfo(tilen) {
+  var result;
+  for (var i = 0; i < tiles.length; i++) {
+    if (tiles[i].id==Math.round(input-0.5)) {
+      result = tiles[i];
+    }
+  }
+  if (input%1!=0) {
+    for (var i = 0; i < result.subs.length; i++) {
+      if (result.subs[i].id==input) {
+        result = result.subs[i];
+      }
+    }
+  }
+  return result;
 }
