704181a7a9cf8e58f6aef32cfea83bbbee853ec8
_̦͍͇_͎͚̰͚_̠̜adri326_̦͍͇_͎͚̰͚_̠̜ <adrien.burgun@orange.fr>
2016-08-10 14:46:10 +0200

    Update core.js

--- create/core.js
+++ create/core.js
@@ -5,6 +5,7 @@ var starter = "../ressources/images/theme/basic-16/";
 var tileSize = 32;
 var oldmap = null;
 loadMap = false;
+var tileSize = 32;
 
 var onSizeChanged = function() {};
 
@@ -13,17 +14,33 @@ function selectSquare(y, x) {
   ax = x;
   ay = y;
 }
-function changeValue() {
-  map[ay][ax] = JSON.parse(document.getElementById('newValue').value);
-  var img = document.getElementById(ay+"-"+ax);
-  img.src = starter+map[ay][ax][0]+".png";
-  img.alt = map[ay][ax][0];
+function selectAll(y, x) {
+  document.getElementById('newValue').value = JSON.stringify(map[y][x]);
+  ax = -1;
+  ay = -1;
 }
-function changeSize() {
+function changeValue() {
+  if (ax>0&&ay>0) { //if only one tile is selected
+    map[ay][ax] = JSON.parse(document.getElementById('newValue').value);
+    var img = document.getElementById(ay+"-"+ax);
+    img.src = starter+map[ay][ax][0]+".png";
+    img.alt = map[ay][ax][0];
+  } else if (ax==-1&&ay==-1) { //if all tiles are selected
+    for (var i = 0; i < map.length; i++) {
+      for (var j = 0; j < map[i].length; j++) {
+        map[i][j] = JSON.parse(document.getElementById('newValue').value);
+        var img = document.getElementById(i+"-"+j);
+        img.src = starter+map[i][j][0]+".png";
+        img.alt = map[i][j][0];
+      }
+    }
+  }
+}
+function changeSize() { //the size changing function (handle the map and the selector)
   w = document.getElementById('width').value;
   h = document.getElementById('height').value;
   var editor = document.getElementById("editor");
-  if (h<oh) {
+  if (h<oh) { //first the height modification
     map = map.slice(0, h);
     for (var i = h; i < oh-1; i++) {
       editor.removeChild(editor.lastChild());
@@ -36,19 +53,25 @@ function changeSize() {
       for (var j = 0; j < w; j++) {
         map[i][j] = [0];
         var xelem = document.createElement("td");
+        xelem.id = "td-"+i+"-"+j;
         var img = document.createElement("img");
         img.src = "../"+setup.imageset+"/0.png";
+        img.id = y+"-"+y;
         img.onclick = "selectSquare("+y+", "+x+");";
-        img.ondblclick = "selectAll();";
+        img.ondblclick = "selectAll("+y+", "+x+");";
         xelem.appendChild(img);
         yelem.appendChild(xelem);
       }
     }
   }
   
-  for (var i = 0; i < h; i++) {
+  for (var i = 0; i < h; i++) { //then the width modification
+    var yelem = document.getElementById("y-"+i);
     if (w<ow) {
       map[i] = map[i].slice(0, w);
+      for (var j = w; j < ow-1; j++) {
+        yelem.removeChild(yelem.lastChild()));
+      }
     } else {
       if (typeof map[i] == "undefined") {
         map[i] = [];
@@ -59,6 +82,17 @@ function changeSize() {
         for (var j = ow; j < w; j++) {
           map[i][j] = [0];
         }
+      }
+      for (var j = ow; j < w; j++) {
+        var xelem = document.createElement("td");
+        xelem.id = "td-"+i+"-"+j;
+        var img = document.createElement("img");
+        img.src = "../"+setup.imageset+"/0.png";
+        img.onclick = "selectSquare("+y+", "+x+");";
+        img.ondblclick = "selectAll("+y+", "+x+");";
+        img.id = y+"-"+y;
+        xelem.appendChild(img);
+        yelem.appendChild(xelem);
       }
     }
   }
@@ -72,7 +106,6 @@ function saveMap() {
   var party = document.getElementById('party').value;
   window.location = 'saveMap.php?party='+party+'&mapId='+mapId+'&map='+JSON.stringify(map);
 }
-var tileSize = 32;
 function init() {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext("2d");
